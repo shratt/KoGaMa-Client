bd0e158592ef7865d498bf6b3a264481f236a53e
shratt <123132403+shratt@users.noreply.github.com>
2024-05-29 11:26:15 -0400

    fix player again

--- launcher.js
+++ launcher.js
@@ -38,7 +38,7 @@ async function launchGame(gameID) {
     }
 
     let gameFrame = document.createElement("iframe");
-    gameFrame.src = location.href + "player/"
+    gameFrame.src = location.origin + location.pathname + "player/"
     gameFrame.style.cssText = "position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;";
     document.write(gameFrame.outerHTML);
 }
