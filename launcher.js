b8f45350053d23272ae5679d84a2c888f91a1ad3
shratt <123132403+shratt@users.noreply.github.com>
2024-05-28 21:22:12 -0400

    bro

--- launcher.js
+++ launcher.js
@@ -40,7 +40,7 @@ async function launchGame() {
     }
 
     let gameFrame = document.createElement("iframe");
-    gameFrame.src = location.href + "/player/"
+    gameFrame.src = location.href + "player/"
     gameFrame.style.cssText = "position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;";
     document.write(gameFrame.outerHTML);
 }
